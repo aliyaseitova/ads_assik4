67e31c1a52519cc6690a16f7ea3d9740a831ff8f
aliyaseitova <230587@astanait.edu.kz>
2024-05-21 18:04:25 +0500

    Created Main class and implemented code from moodle

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyGraph myGraph = new MyGraph(5);
+        myGraph.addEdge(0,1);
+        myGraph.addEdge(1,2);
+        myGraph.addEdge(2,4);
+        myGraph.addEdge(3,4);
+        myGraph.addEdge(3,1);
+
+        System.out.println(myGraph.hasEdge(2, 3));
+        System.out.println(myGraph.hasEdge(2, 4));
+        myGraph.removeEdge(2, 4);
+        System.out.println(myGraph.hasEdge(2,4));
+
+        System.out.println(myGraph.getNeighbors(1));
+
+        myGraph.printGraph();
+
+    }
+}
