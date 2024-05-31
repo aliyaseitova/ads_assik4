d2120f3dd0d2ce96c8319614174228f2c0fda491
aliyaseitova <230587@astanait.edu.kz>
2024-05-31 16:28:41 +0500

    implementing BreadthFirstSearch.java class code

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,22 +1,21 @@
-public class BreadthFirstSearch extends Search {
+import java.util.*;
 
-    public BreadthFirstSearch(Vertex source) {
-        super(source);
+public class WeightedGraph {
+    private final Map<Vertex, List<Edge>> adjVertices = new HashMap<>();
+
+    public void addVertex(Vertex vertex) {
+        adjVertices.putIfAbsent(vertex, new ArrayList<>());
     }
-    public void search(WeightedGraph graph) {
-        Queue<Vertex> queue = new LinkedList<>();
-        marked.add(source);
-        queue.add(source);
-        while (!queue.isEmpty()) {
-            Vertex v = queue.poll();
-            for (WeightedGraph.Edge edge : graph.getEdges(v)) {
-                Vertex w = edge.getDestination();
-                if (!marked.contains(w)) {
-                    marked.add(w);
-                    edgeTo.put(w, v);
-                    queue.add(w);
-                }
-            }
-        }
+
+    public void addEdge(Vertex src, Vertex dest, int weight) {
+        adjVertices.get(src).add(new Edge(dest, weight));
+        adjVertices.get(dest).add(new Edge(src, weight)); // For undirected graph
     }
-}
+
+    public List<Vertex> getVertices() {
+        return new ArrayList<>(adjVertices.keySet());
+    }
+
+    public List<Edge> getEdges(Vertex vertex) {
+        return adjVertices.get(vertex);
+    }
