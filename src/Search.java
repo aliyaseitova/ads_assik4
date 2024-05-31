132958294f220803251703509ccd24248221a62a
aliyaseitova <230587@astanait.edu.kz>
2024-05-31 16:34:58 +0500

    deleting the whole code of Search.java class and writing a new one in orde to fix mistakes

--- src/Search.java
+++ src/Search.java
@@ -1,27 +1,5 @@
-import java.util.*;
+import java.util.List;
 
-public abstract class Search {
-    protected Set<Vertex> marked;
-    protected Map<Vertex, Vertex> edgeTo;
-    protected final Vertex source;
-}
-public Search(Vertex source) {
-    this.source = source;
-    this.marked = new HashSet<>();
-    this.edgeTo = new HashMap<>();
-}
-public boolean hasPathTo(Vertex v) {
-    return marked.contains(v);
-}
-public List<Vertex> pathTo(Vertex v) {
-    if (!hasPathTo(v)) return null;
-    List<Vertex> path = new ArrayList<>();
-    for (Vertex x = v; x != null; x = edgeTo.get(x)) {
-        path.add(x);
-    }
-    Collections.reverse(path);
-    return path;
-}
-
-public abstract void search(WeightedGraph graph);
+public interface Search<V> {
+    List<V> pathTo(V goal);
 }
