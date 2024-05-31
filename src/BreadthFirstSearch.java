bc8e9e12519c6d3eb8c866c5c9dd617b8926d882
aliyaseitova <230587@astanait.edu.kz>
2024-05-31 15:55:19 +0500

    implementing BFS class code that extends Search class

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,2 +1,22 @@
-public class BreadthFirstSearch {
+public class BreadthFirstSearch extends Search {
+
+    public BreadthFirstSearch(Vertex source) {
+        super(source);
+    }
+    public void search(WeightedGraph graph) {
+        Queue<Vertex> queue = new LinkedList<>();
+        marked.add(source);
+        queue.add(source);
+        while (!queue.isEmpty()) {
+            Vertex v = queue.poll();
+            for (WeightedGraph.Edge edge : graph.getEdges(v)) {
+                Vertex w = edge.getDestination();
+                if (!marked.contains(w)) {
+                    marked.add(w);
+                    edgeTo.put(w, v);
+                    queue.add(w);
+                }
+            }
+        }
+    }
 }
