053017a368bd5b3c0b2ee2811444e0e53cc3c1bf
aliyaseitova <230587@astanait.edu.kz>
2024-05-31 16:50:50 +0500

    added vertices to the graph

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
         Vertex b = new Vertex("2", "Besh");
         Vertex c = new Vertex("3", "Astana");
 
+
+        WeightedGraph graph = new WeightedGraph();
+        graph.addVertex(a);
+        graph.addVertex(b);
+        graph.addVertex(c);
+
     }
 }
