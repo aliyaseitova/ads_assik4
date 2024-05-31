8a709b422682582b5a3372f8f649a050c408bad2
aliyaseitova <230587@astanait.edu.kz>
2024-05-31 16:57:07 +0500

    adding weights and printing paths

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,16 @@ public class Main {
         graph.addVertex(b);
         graph.addVertex(c);
 
+        graph.addEdge(a, b, 4);
+        graph.addEdge(a, c, 2);
+        graph.addEdge(b, c, 5);
+
+        BreadthFirstSearch bfs = new BreadthFirstSearch();
+        bfs.bfs(graph, a);
+        System.out.println("BFS path from A to C: " + bfs.pathTo(c));
+
+        DijkstraSearch dijkstra = new DijkstraSearch(graph, a);
+        System.out.println("Dijkstra path from A to C: " + dijkstra.pathTo(c));
+        System.out.println("Dijkstra distance from A to C: " + dijkstra.distTo(c));
     }
 }
