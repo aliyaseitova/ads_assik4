ef2de6604b82be7c1a881031bc40d05c6d5559fb
aliyaseitova <230587@astanait.edu.kz>
2024-05-31 16:31:41 +0500

    Changed DijkstraSearch class back to what it was initially

--- src/DijkstraSearch.java
+++ src/DijkstraSearch.java
@@ -5,23 +5,20 @@ public class DijkstraSearch {
     private final Map<Vertex, Vertex> edgeTo = new HashMap<>();//map to store paths
     private  final PriorityQueue<Vertex> pq;//algorithm priority queue;
 
-    public DijkstraSearch(Vertex source) {
-        super(source);
-        distTo = new HashMap<>();
-        pq = new PriorityQueue<>(Comparator.comparingInt(distTo::get));
-
+    public DijkstraSearch(WeightedGraph graph, Vertex source){
+        pq = new PriorityQueue<>(Comparator.comparingInt(distTo::get));//initialize priority queue with comparator
+        for (Vertex v : graph.getVertices()) {
+            distTo.put(v, Integer.MAX_VALUE);//initializing all distances to infinity
+        }
         distTo.put(source, 0);
-        pq.add(source);
-    }
-    public void search(WeightedGraph graph) {
-        while (!pq.isEmpty()) {
-            Vertex v = pq.poll();
-            for (WeightedGraph.Edge edge : graph.getEdges(v)) {
-                relax(edge, v);
+        pq.add(source);//adding source to the priority queue(pq)
+        while (!pq.isEmpty()){
+            Vertex v = pq.poll();//removing vertex with the smallest dist
+            for (WeightedGraph.Edge edge : graph.getEdges(v)){
+                relax(edge, v);//relax edges from the vertex
             }
         }
     }
-
 
     private void relax(WeightedGraph.Edge edge, Vertex v){
         Vertex w = edge.getDestination();//gets the dest vertex of the edge
@@ -39,6 +36,19 @@ public class DijkstraSearch {
     }
     public int distTo(Vertex v){
         return distTo.get(v);
+    }
+    public boolean hasPathTo(Vertex v){
+        return distTo.get(v) < Integer.MAX_VALUE;//checks if there is path to a vertex
+    }
+    public List<Vertex> pathTo(Vertex v) {
+        if (!hasPathTo(v)) return null;//returns null, if there is no path
+        List<Vertex> path = new ArrayList<>();
+        for (Vertex x = v; x != null; x = edgeTo.get(x)){
+            path.add(x);
+            /*constructs the path from end to start and adds  vertex to the path*/
+        }
+        Collections.reverse(path);//reversing the path
+        return path;
     }
 }
 
